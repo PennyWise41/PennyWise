02c0968a827554228725f6ddb53a7c4320e78cde
kfiddle <kenjfiddle@gmail.com>
2023-08-15 13:52:24 -0500

    more test

--- __tests__/react.js
+++ __tests__/react.js
@@ -4,12 +4,32 @@ import userEvent from '@testing-library/user-event'
 import { render, screen, waitFor } from '@testing-library/react';
 import regeneratorRuntime from 'regenerator-runtime';
 
-import App from '..client/App.jsx';
+import App from '../client/App.jsx';
 
 describe('Unit testing React components', () => {
     let num = 7;
     it('this num should equal 7', () => {
         expect(num).toEqual(7);
     });
+    it('should have some text', () => {
+        let someText = 'some text';
+        expect(typeof someText === 'string').toBe(true)
+        expect(typeof someText).toBe('string');
+    })
+});
+
+describe('more random testing', () => {
+    let text;
+
+    let props = { userName: 'serena', password: '12345' };
+    beforeAll(() => {
+        text = render(<div>{props.userName}</div>);
+    });
+
+    test('this will test a div', () => {
+       expect(text).not.toBe(null)
+    })
+
+
 })
 
